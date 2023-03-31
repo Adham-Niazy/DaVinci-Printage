e8388af3b98d8e061103f94b896091937e676475
Adham-Niazy <adhamniazy@gmail.com>
2023-03-31 21:37:12 +0200

    Adding Customizer page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <main className="app transition-all ease-in">
       <Home />
+      <Customizer />
     </main>
   )
 }
