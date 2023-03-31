24f84a4fa81adeca2f5acb3d1f98f70f35b7d5ee
Adham-Niazy <adhamniazy@gmail.com>
2023-03-31 17:43:11 +0200

    Configure tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
