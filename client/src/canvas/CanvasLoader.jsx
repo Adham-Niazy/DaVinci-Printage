037735b203e1b9d44c29f386fa287f4966b4c674
Adham-Niazy <adhamniazy@gmail.com>
2023-04-01 06:27:25 +0200

    Preparing Canvas Loader for Later

---
+++ client/src/canvas/CanvasLoader.jsx
@@ -0,0 +1,13 @@
+import { Html, useProgress } from "@react-three/drei";
+
+function CanvasLoader() {
+  const { progress } = useProgress();
+
+  return (
+    <Html>
+      
+    </Html>
+  )
+}
+
+export default CanvasLoader
