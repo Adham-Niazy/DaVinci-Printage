2a51f758928d75e72ecbae4f209fbbb2c2611505
Adham-Niazy <adhamniazy@gmail.com>
2023-04-01 07:22:04 +0200

    Adding download shirt image

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -159,6 +159,13 @@ function Customizer() {
                 handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
+            <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button>
           </motion.div>
         </>
       )}
