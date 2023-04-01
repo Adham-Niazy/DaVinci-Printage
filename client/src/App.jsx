1c26096208285caf422504be474548c0889e90b1
Adham-Niazy <adhamniazy@gmail.com>
2023-04-01 05:32:27 +0200

    Adding React Toaster for alerts to the user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
+import { Toaster } from 'react-hot-toast';
+
 import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 import Canvas from "./canvas";
 
 function App() {
   return (
-    <main className="app transition-all ease-in">
-      <Home />
-      <Canvas />
-      <Customizer />
-    </main>
+    <>
+      <main className="app transition-all ease-in">
+        <Home />
+        <Canvas />
+        <Customizer />
+      </main>
+      <Toaster />
+    </>
   )
 }
 
