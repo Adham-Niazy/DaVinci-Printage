3db1fcd3f1019f3a8fa4d0e72849e4bbfc5b4b96
Adham-Niazy <adhamniazy@gmail.com>
2023-04-01 04:41:54 +0200

    making custom button bahve smartly with colors + adding outline type of buttons

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useSnapshot } from 'valtio';
 
 import state from '../store';
-import { useSnapshot } from 'valtio';
+import { getContrastingColor } from '../config/helpers';
 
 function CustomButton({ title, type, customStyles, handleClick }) {
     const snap = useSnapshot(state)
@@ -9,8 +10,14 @@ function CustomButton({ title, type, customStyles, handleClick }) {
         if ( type === 'filled' ) {
             return {
                 backgroundColor: snap.color,
-                color: '#fff'
+                color: getContrastingColor(snap.color)
             }
+        } else if ( type === 'outline' ) {
+          return {
+            borderWidth: '1px',
+            borderColor: snap.color,
+            color: snap.color 
+          }
         }
     }
   return (
